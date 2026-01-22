676bea76cdb8c6b4796c878c645f3ffe9474483f
Hugo Garcia <105685336+hugogvc@users.noreply.github.com>
2026-01-22 02:57:33 -0300

    Resoluão de Exércicos

---
+++ Exercicios Sessão 7/Questão01.py
@@ -0,0 +1,11 @@
+'''
+
+1. Crie um programa que tenha uma função que recebe um parâmetro inteiro e devolve o seu dobro
+
+'''
+def dobro(num):
+    return num*2
+
+print(dobro (int(input("digite um numero inteiro"))))
+
+
---
+++ Exercicios Sessão 7/Questão02.py
@@ -0,0 +1,6 @@
+'''
+Faça um programa que tenha uma função que recebe uma data no formato string exemplo “01/01/2024” e
+imprima ela por extenso como “1 de janeiro de 20204”
+
+'''
+
